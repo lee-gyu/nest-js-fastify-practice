3560b4163380f791db3c3ec5693db8e1cb555a7d
Lee <leegyu4dev@gmail.com>
2025-11-29 22:42:38 +0900

    refactor: update import paths and formatting for consistency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
+import {
+    type NestFastifyApplication,
+    FastifyAdapter,
+} from '@nestjs/platform-fastify';
+import { join } from 'node:path';
+import handlebars from 'handlebars';
+
+// for ESM
+const __dirname = new URL('.', import.meta.url).pathname;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+
+    app.useStaticAssets({
+        root: join(__dirname, '..', 'public'),
+        prefix: '/public/',
+    });
+    app.setViewEngine({
+        engine: {
+            handlebars,
+        },
+        templates: join(__dirname, '..', 'views'),
+    });
+
+    await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap().catch((error) => console.error(error));
