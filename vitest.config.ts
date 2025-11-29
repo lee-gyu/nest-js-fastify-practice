abe08dbe3908717e0bcd4c448114c5a1be5e713f
Lee <leegyu4dev@gmail.com>
2025-11-29 22:52:24 +0900

    fix: update tsconfig.build.json formatting and add vitest configuration file
    - Adjusted indentation in tsconfig.build.json for consistency.
    - Added vitest.config.ts to configure Vitest for testing with SWC and TypeScript path aliases.

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+    test: {
+        include: ['src/**/*.spec.ts', '**/*.e2e-spec.ts'],
+        globals: true,
+        root: './',
+        coverage: {
+            provider: 'v8',
+        },
+    },
+    plugins: [
+        // This is required to build the test files with SWC
+        swc.vite({
+            // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+            module: { type: 'es6' },
+        }),
+    ],
+    resolve: {
+        alias: {
+            // Ensure Vitest correctly resolves TypeScript path aliases
+            src: resolve(process.cwd(), 'src/'),
+        },
+    },
+});
